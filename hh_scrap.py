86f10b5fb83a1f786697dcc4900fac90a4f9e900
eugiv <eugene.ivanov@ymail.com>
2023-12-19 07:04:25 +0300

    midprogress

--- hh_scrap.py
+++ hh_scrap.py
@@ -1,21 +1,38 @@
-import re
-
 import requests
 from fake_headers import Headers
+from bs4 import BeautifulSoup
 
 head_gen = Headers(os='win', browser='chrome')
 
-search_keywords = ['flask', 'django']
+search_keywords = ["python", "flask", "django"]
+search_keywords = "+".join(search_keywords)
 
-url = f'https://hh.ru/search/vacancy?text=&area=1&area=2'
-
-response = requests.get(url, headers=head_gen.generate())
-print(response)
-
-def search_formatter(search_list: list):
-    
-    search_pattern = r""
+url_init = f'https://hh.ru/search/vacancy?text={search_keywords}&area=1&area=2'
 
 
-if __name__ == "__main__":
-    search_formatter(search_keywords)
+def cook_soup(url_soup: str):
+    response = requests.get(url_soup, headers=head_gen.generate())
+    html_pages = response.text
+    hot_soup = BeautifulSoup(html_pages, 'lxml')
+    return hot_soup
+
+
+soup_pages = cook_soup(url_init)
+pages = list(soup_pages.find('div', class_='pager').text)
+pages = [x for x in pages if x.isdigit()]
+
+for page in pages:
+    url = f'https://hh.ru/search/vacancy?text={search_keywords}&area=1&area=2&page={int(page)-1}'
+    soup = cook_soup(url)
+
+    main_block = soup.find_all('div', class_='serp-item')
+
+    for element in main_block:
+        link = element.find('a')['href']
+        print(link)
+
+
+
+
+# if __name__ == "__main__":
+#     search_formatter(search_keywords)
