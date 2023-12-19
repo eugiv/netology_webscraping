e39ffaa327eb72a60c7e09d546d27f3d51ef4ac4
eugiv <eugene.ivanov@ymail.com>
2023-12-19 07:35:10 +0300

    midprogress

--- hh_scrap.py
+++ hh_scrap.py
@@ -2,20 +2,20 @@ import requests
 from fake_headers import Headers
 from bs4 import BeautifulSoup
 
-head_gen = Headers(os='win', browser='chrome')
-
 search_keywords = ["python", "flask", "django"]
 search_keywords = "+".join(search_keywords)
 
-url_init = f'https://hh.ru/search/vacancy?text={search_keywords}&area=1&area=2'
-
 
 def cook_soup(url_soup: str):
+    head_gen = Headers(headers=True)
+
     response = requests.get(url_soup, headers=head_gen.generate())
-    html_pages = response.text
-    hot_soup = BeautifulSoup(html_pages, 'lxml')
+    html = response.text
+    hot_soup = BeautifulSoup(html, 'lxml')
     return hot_soup
 
+
+url_init = f'https://hh.ru/search/vacancy?text={search_keywords}&area=1&area=2'
 
 soup_pages = cook_soup(url_init)
 pages = list(soup_pages.find('div', class_='pager').text)
@@ -23,13 +23,16 @@ pages = [x for x in pages if x.isdigit()]
 
 for page in pages:
     url = f'https://hh.ru/search/vacancy?text={search_keywords}&area=1&area=2&page={int(page)-1}'
+    print(url)
     soup = cook_soup(url)
-
     main_block = soup.find_all('div', class_='serp-item')
 
+    count = 0
     for element in main_block:
         link = element.find('a')['href']
+        count += 1
         print(link)
+        print(count)
 
 
 
